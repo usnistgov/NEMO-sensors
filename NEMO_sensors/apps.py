0bbd24a822c1ed38720e08583ea582efd98c2dd3
mrampant <mathieu.rampant@nist.gov>
2025-02-14 11:13:57 -0500

    - updated NEMO and NEMO-CE dependencies to version 7.0.0 - added dependency check in app initialization

--- NEMO_sensors/apps.py
+++ NEMO_sensors/apps.py
@@ -7,7 +7,9 @@ class SensorsConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
+        from NEMO.plugins.utils import check_extra_dependencies
+
         """
         This code will be run when Django starts.
         """
-        pass
+        check_extra_dependencies(self.name, ["NEMO", "NEMO-CE"])
