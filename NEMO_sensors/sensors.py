9f06d193a0ad3e276423b656dadbdbbae3dd1aeb
mrampant <mathieu.rampant@nist.gov>
2026-06-04 08:17:42 -0400

    - release 1.2.1 - fixed slave -> device_id with new version of pymodbus

--- NEMO_sensors/sensors.py
+++ NEMO_sensors/sensors.py
@@ -174,7 +174,7 @@ class ModbusTcpSensor(Sensor):
                 raise Exception(
                     f"Connection to server {sensor.card.server}:{sensor.card.port} could not be established"
                 )
-            kwargs = {"slave": sensor.unit_id} if sensor.unit_id is not None else {}
+            kwargs = {"device_id": sensor.unit_id} if sensor.unit_id is not None else {}
             read_response = client.read_holding_registers(sensor.read_address, count=sensor.number_of_values, **kwargs)
             if read_response.isError():
                 raise Exception(f"Error with sensor {sensor.name}: {str(read_response)}")
